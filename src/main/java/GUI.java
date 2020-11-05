43c94166a6d44c695178e06e4fd0a96f7e3c0880
MELANCHOLY_WU <1666315117@qq.com>
2020-11-05 18:44:43 +0800

    1

---
+++ src/main/java/GUI.java
@@ -0,0 +1,53 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+public class GUI {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setSize(1200,1200);
+        f.setLocation(200,200);
+        f.setLayout(null);
+        //添加文字
+//        JLabel l = new JLabel("文字");
+//        l.setForeground(Color.red);
+//        l.setBounds(50,50,280,30);
+//        f.add(l);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        //添加图片
+//        f.setVisible(true);
+//        JLabel l = new JLabel();
+//        ImageIcon i = new ImageIcon("D:/图片/收藏/IMG_3035.PNG");
+//        l.setIcon(i);
+//        //label的大小设置为ImageIcon,否则显示不完整;
+//        l.setBounds(50,50,i.getIconWidth(),i.getIconHeight());
+//        f.add(l);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        f.setVisible(true);
+        //添加按钮
+//        JButton bt = new JButton("Start");
+//        bt.setForeground(Color.pink);
+//
+//        bt.setBounds(50,50,500,250);
+//        f.add(bt);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        f.setVisible(true);
+        //复选框
+//        JCheckBox cb1 = new JCheckBox("选择1");
+//        JCheckBox cb2 = new JCheckBox("选择2");
+//        //设置cb1默认选中
+//        cb1.setSelected(true);
+//        cb1.setBounds(50,50,130,30);
+//        cb2.setBounds(50,100,130,30);
+//        //判断是否被选中
+//        System.out.println(cb2.isSelected());
+//        f.add(cb1);
+//        f.add(cb2);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        f.setVisible(true);
+
+
+    }
+}
